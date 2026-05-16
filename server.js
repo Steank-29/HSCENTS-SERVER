29957f6b82c99f7b61947e403aa71a3a2504c86e
SamiJelassi <samijlassi2909@gmail.com>
2026-05-16 15:40:02 +0200

    first commit

--- server.js
+++ server.js
@@ -312,7 +312,7 @@ app.use((err, req, res, next) => {
 // SERVER STARTUP
 // ======================
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 
 const server = app.listen(PORT, () => {
   console.log(`
